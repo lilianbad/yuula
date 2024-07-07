8dab34c0b8db7df449c460a3dca5b3b0e6a75784
Badios <88975469+lilianbad@users.noreply.github.com>
2024-07-07 23:11:19 +0100

    Add files via upload

---
+++ js/js/main.js
@@ -0,0 +1,29 @@
+
+!(function($) {
+  "use strict";
+  // Intro carousel
+  var heroCarousel = $("#heroCarousel");
+
+  heroCarousel.on('slid.bs.carousel', function(e) {
+    $(this).find('h2').addClass('animated fadeInDown');
+    $(this).find('p').addClass('animated fadeInUp');
+    $(this).find('.btn-get-started').addClass('animated fadeInUp');
+  });
+  
+  // Initiate the venobox plugin
+  $(window).on('load', function() {
+    $('.venobox').venobox();
+  });
+
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+  // Initi AOS
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out-back"
+  });
+
+})(jQuery);
